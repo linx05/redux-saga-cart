7e0ce1b780a495a8c2b46d39b8733a490964a2dd
Cesar Laredo <cesar.f.laredo@gmail.com>
2018-01-26 14:55:25 -0700

    getUserSaga and initial sagas config

--- src/getStore.js
+++ src/getStore.js
@@ -1,37 +1,43 @@
 import {
-    createStore,
-    applyMiddleware,
-    compose
+  createStore,
+  applyMiddleware,
+  compose
 } from 'redux';
 
 import { createLogger } from 'redux-logger';
 import { Iterable } from 'immutable'
+import createSagaMiddleware from 'redux-saga';
 import thunk from 'redux-thunk'
 
 import { getQuery } from './utility'
+import { initSagas } from './initSagas';
 import { reducer } from './combineReducers';
 import { defaultState } from './defaultState'
 
 const stateTransformer = (state) => {
-    if (Iterable.isIterable(state)) return state.toJS();
-    else return state;
+  if (Iterable.isIterable(state)) return state.toJS();
+  else return state;
 };
 
 const logger = createLogger({
-    stateTransformer,
+  stateTransformer,
 });
 
 export const getStore = ()=>{
-    const middleWares = [thunk];
-    if (getQuery()['logger']) { middleWares.push(logger)}
-    const composables = [applyMiddleware(...middleWares)]
-    const enhancer = compose(
-        ... composables
-    );
-    const store = createStore(
-        reducer,
-        defaultState,
-        enhancer
-    );
-    return store;
+  const sagaMiddleware = createSagaMiddleware();
+  const middleWares = [sagaMiddleware,thunk];
+  if (getQuery()['logger']) { middleWares.push(logger)}
+  const composables = [applyMiddleware(...middleWares)
+    //    , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ];
+  const enhancer = compose(
+    ... composables
+  );
+  const store = createStore(
+    reducer,
+    defaultState,
+    enhancer,
+  );
+  initSagas(sagaMiddleware);
+  return store;
 };
