d757ca1b15065842f5a90b350bbc3b16efa5e51d
Cesar Laredo <cesar.f.laredo@gmail.com>
2018-01-26 17:16:02 -0700

    Obtain the price of items through the use of all, take and call

---
+++ src/sagas/itemPriceSaga.js
@@ -0,0 +1,23 @@
+import { take, all, call, put } from 'redux-saga/effects';
+import fetch from 'isomorphic-fetch';
+import {
+  SET_CURRENT_USER,
+  SET_CART_ITEMS,
+  setItemPrice,
+} from '../actions';
+
+function* fetchItemPrice(id, currency) {
+  const response = yield fetch(`http://localhost:8081/prices/${currency}/${id}`);
+  const data = yield response.json();
+  const price = data[0].price;
+  yield put(setItemPrice(id, price));
+}
+
+export function* itemPriceSaga() {
+  const [{ user }, { items }] = yield all([
+    take(SET_CURRENT_USER),
+    take(SET_CART_ITEMS),
+  ]);
+  yield items.map(item => call(fetchItemPrice, item.id, user.country));
+
+}
